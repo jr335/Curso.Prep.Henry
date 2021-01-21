7ea877344b4034e208414e4e8a3a8042a4048cf9
aqui el nombre <aqui el mail>
2021-01-21 13:46:45 -0300

    comentario

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,22 +1,22 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+var nuevaString = 'Nombre y Apellido';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+var nuevoNum = 08012021;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+var nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+var nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+var nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+var nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -25,120 +25,242 @@ const nuevoModulo = 21 % 5 === null;
 // Pista: "console.log()" NO fucionará.
 // No cambies los nombres de las funciones.
 
-function devolverString(str) {
+function devolverString() {
   // "Return" la string provista: str
   // Tu código:
+  
+  var str= str.length;
+  return str;
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
+  x = 10;
+  y = 5;
+  var resultado = x + y;
+  return resultado;
+
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
+  x = 10;
+  y = 5;
+  var totalresta = x - y;
+  return totalresta;
+
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
+  x = 10;
+  y = 2;
+  var producto = x * y;
+  return producto;
+
+  
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
+  var division = x / y;
+  return division;
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
+ if (x = y)
+ {
+   return true;
+ }
+ return false;
 }
 
-function tienenMismaLongitud(str1, str2) {
+function tienenMismaLongitud() {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-}
+   var str1 = ("palabra");
+   var str2 =("frase");
+  if (str1 = str2)
+  {
+    return true;
+  } 
+    return false;
+  
 
-function menosQueNoventa(num) {
+  }
+ 
+
+
+function menosQueNoventa() {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
+  argumento = 80
+  num > 50;
+  if (argumento < 90){
+
+    return true;
+  }
+  return false;  
+
 }
 
-function mayorQueCincuenta(num) {
+
+
+
+
+
+function mayorQueCincuenta() {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
+  argumento = 80
+  num > 50;
+  if (argumento > num)
+  {
+ return true;
+  }
+  return false;  
+
+
 }
 
-function obtenerResto(x, y) {
+function obtenerResto() {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
+  x = 25;
+  y = 4;
+ resto =(x % y === 1)
+ {
+  return resto;
+ }
+ 
+
 }
 
-function esPar(num) {
+function esPar() {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
+ num = 4;
+ if (num = num * 2)
+ {
+   return true;
+ }
+ return false;
 }
 
-function esImpar(num) {
+
+
+function esImpar() {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
+ num = 4;
+ numImpar = (num = num + 1);
+ if (numImpar = 5)
+ {
+ return true;
+
+ }
+return false;
 }
 
-function elevarAlCuadrado(num) {
+function elevarAlCuadrado() {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
+   num = 50;
+   cuadrado = (num * 2);
+   return cuadrado;
+
 }
 
-function elevarAlCubo(num) {
+function elevarAlCubo() {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
+ num = 30;
+ cuboNum = (num * 3)
+ return cuboNum;
+
 }
 
-function elevar(num, exponent) {
+
+function elevar() {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-}
+ num = 30;
+ exponent = 2;
+ elevado= Math.pow(num, exponent);
+    return elevado;
+ }
 
-function redondearNumero(num) {
+function redondearNumero() {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
+num = 6,9998
+redondear = Math.round(num);
+return redondear;
+
 }
 
-function redondearHaciaArriba(num) {
+function redondearHaciaArriba() {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
+  num = 6,9999;
+  haciarriba = Math.ceil(num);
+  return haciarriba;
 }
 
-function agregarSimboloExclamacion(str) {
+function agregarSimboloExclamacion() {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  str ='hello world';
+  saludo = '!';
+  nuevaString = str + saludo;
+  return nuevaString;
 }
 
-function combinarNombres(nombre, apellido) {
+function combinarNombres() {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
+ str = 'Nombre';
+ str2 = "Apellido";
+ Combinados = (str +' '+ str2);
+ return Combinados;
 }
 
-function obtenerSaludo(nombre) {
+function obtenerSaludo() {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+ strSaludo = 'Hola';
+ nombre = 'Jorge';
+ apellido = 'Fernández';
+ unir = (`${strSaludo} ${nombre}${apellido}`);
+ return unir; 
+
 }
 
-function obtenerAreaRectangulo(alto, ancho) {
+function obtenerAreaRectangulo() {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
+ altura = 5;
+ ancho = 10;
+ area = (ancho * altura);
+ return area;
+  
 }
 
 
--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,16 +3,24 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un array
   // Tu código:
-}
+  const array = [ 'Martin', 'Antonio', 'Sara', 'Samuel'];
+  return array [0];
+{
+
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  const array = [ 'Martin', 'Antonio', 'Sara', 'Samuel'];{
+  return (array[array.length - 1]);
 }
-
+}
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  const array = [ 'Martin', 'Antonio', 'Sara', 'Samuel'];
+  largo = array.length;
+  return largo;
 }
 
 function incrementarPorUno(array) {
@@ -20,12 +28,23 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  var array = [ 1, 2, 3, 4];
+  x = 0;
+    
+  for (var i = 0; i < array.length; i++) {
+    otroarray= (array[i]+1) }
+    return(otroarray);
 }
+  
 
 function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+array = ['Julio','Sofia','Juan'];
+array.push = ('Marcelo');
+array [array.length] = elemento;
+return array;
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
@@ -33,7 +52,14 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  
+  array = ('Julio','Sofia','Juan');
+  elemento = 0
+  array.unshift = ('Marcelo');
+  array.unshift = elemento;
+  return array;
 }
+
 
 function dePalabrasAFrase(palabras) {
   // "palabras" es un array de strings/cadenas
@@ -41,30 +67,75 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+ palabras = ['welcome', 'to', 'Henry'];
+  return palabras.join (' ');
 }
+
 
 function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  array = ['']
+  elemento = "Hola"
+  for(var i = 0; i < array.length; i++) {
+    if(array[i] === elemento) {
+      return true;
+    }
+  }
+  return false;
 }
+
+
+
 
 function agregarNumeros(numeros) {
   // "array" debe ser una matriz de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  var suma = 0;
+  for(var i = 0; i < numeros.length; i++) {
+    suma = suma + numeros[i];
+  }
+  return suma;
 }
+
+
+
+
 
 function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
-}
+     var resultadosTest = [1, 5, 7, 10];{
+     suma = 0;
+    for(i = 0; i < resultadosTest.length; i++){
+        porcentaje = (suma = suma + numeros[i]); 
+           porcentaje = porcentaje % 4; 
+              return porcentaje;
+  }
+ }
+
 
 function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+ numeros = [1, 5, 7, 10];{
+  
+  var mayor = numeros[0];
+   
+  for(i = 0; i < numeros.length; i++){
+      if (numeros[i] > mayor){
+          mayor = numeros[i];
+      }
+  }
+   return (mayor);
+   
+ }
+  
+
 }
 
 function multiplicarArgumentos() {
@@ -72,7 +143,14 @@ function multiplicarArgumentos() {
   // Si no se pasan argumentos devuelve 0
   // Si se pasa un argumento, simplemente devuélvelo
   // Tu código:
+  if(arguments.length < 1) return 0;
+  var producto = 1;
+  for(var i = 0; i < arguments.length; i++) {
+    producto = producto * arguments[i];
+  }
+  return producto;
 }
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
@@ -90,4 +168,4 @@ module.exports = {
   promedioResultadosTest,
   numeroMasGrande,
   multiplicarArgumentos,
-};
+  };
